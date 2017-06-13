6c56280be5dca7dbb89df663119998a9204a3a8a
Hao <l_hao@aol.com>
2017-06-13 16:14:56 +0800

    Add StreamingAssets for loading MovieTexture

---
+++ Assets/StreamingAssetTEST/StreamingAssetTEST.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class StreamingAssetTEST : MonoBehaviour {
+
+    Renderer rend;
+    string url;
+
+    private void Awake()
+    {
+        rend = GetComponent<Renderer>();
+        url = "file:///" + Application.streamingAssetsPath + "/EnvMov.mp4";
+        Debug.Log(url);
+    }
+    IEnumerator Start()
+    {
+        WWW www = new WWW(url);
+        MovieTexture movieTexture = www.movie;
+        while (!movieTexture.isReadyToPlay)
+        {
+            yield return null;
+        }
+        
+        rend.material.mainTexture = movieTexture;
+
+        movieTexture.Play();
+
+    }
+}
+
+
+
